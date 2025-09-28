97bd84ad3d17a7500c30258f472c26b107ef9e2c
Priyanshu Kumar <krpriyanshu79858@gmail.com>
2025-09-29 01:29:28 +0530

    test route

--- server.js
+++ server.js
@@ -64,5 +64,10 @@ require('./socketHandler')(io);
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/ping', (req, res) => {
+  res.json("PONG")
+});
+
+
 // 5. Start the HTTP server instead of the Express app
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
